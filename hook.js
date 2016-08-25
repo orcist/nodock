8814ec9f79bfabd13620a4e58aaaed3141be774f
Kubo Motyl <motyl.kubo@gmail.com>
2016-08-25 17:55:50 +0200

    set utf8 std encoding

--- hook.js
+++ hook.js
@@ -35,10 +35,13 @@ github.on('pull_request:' + PROJECT.repository, function(ref, data) {
   ) {
     var args = unpackArgs(DEPLOY_ARGS)
     console.log('running', './deploy.sh ' + [PROJECT.name, PROJECT.path, args].join(' '))
+
     var child = spawn('sh', ['./deploy.sh', PROJECT.name, PROJECT.path, args])
+    child.stdout.setEncoding('utf8')
     child.stdout.on('data', function(data) {
         console.log(data)
     })
+    child.stderr.setEncoding('utf8')
     child.stderr.on('data', function(error) {
         console.log(error)
     })
